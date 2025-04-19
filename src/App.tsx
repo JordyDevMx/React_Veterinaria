ee0664228debfde1531507f8fffbf45a73991edd
Jordy <112351586+jordy291093@users.noreply.github.com>
2025-04-19 12:27:24 -0600

    Add logo and link

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,11 @@ function App() {
 
   return (
     <>
+    
+      <header className="flex justify-center items-center py-4 px-4">
+        <img className="w-70" src="/logo.svg" alt="Logo" />
+      </header>
+
       <div className="container mx-auto mt-20">
         <h1 className="text-5xl font-black text-center md:w-2/3 md:mx-auto">
           Seguimiento de pacientes{' '}
@@ -26,7 +31,7 @@ function App() {
       <ToastContainer/>
 
       <footer className="bg-red-600 p-4 text-center mt-16">
-        <p className="text-2xl text-white">&copy; {currentYear} <a className="font-black" href="https://jordyweb.site/" target="blank" rel="noopener noreferrer">JordyDev</a> | Todos los derechos reservados</p>
+        <p className="text-2xl text-white">&copy; {currentYear} <a className="font-black" href="https://jordydev.website/" target="blank" rel="noopener noreferrer">JordyDev</a> | Todos los derechos reservados</p>
       </footer>
     </>
   )
